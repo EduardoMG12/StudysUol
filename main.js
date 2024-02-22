c148998dd08627c7f4eb2e22efe6677fcc9a0272
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-02-22 17:07:06 -0300

    refactor(main): create 'if' and 'else' schema, finalized project seven

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ celcius = prompt("Enter the value in celsius: ")
 let mathematicalEquationConvertCelciusToFahrenheit = celcius * 1.8 + 32
 
 alert("celcius: " + celcius + " is igual "+ mathematicalEquationConvertCelciusToFahrenheit.toFixed(2) +" in fahrenheit ")
+
+// project 8
+
+let grade = prompt("Enter the student's grade")
+
+if(0<grade<100){
+    alert("invalid grade!!")
+}
+
+else if(90<= grade){
+    alert("the student got an A")
+}
+else if(80<= grade){
+    alert("the student got an F")
+}
+else if(70<= grade){
+    alert("the student got an C")
+}
+else if(60<= grade){
+    alert("the student got an D")
+}else alert("the student got an F")
