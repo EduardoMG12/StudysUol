868954824ccef08e12b48005240c38f428debee2
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-02-22 15:09:10 -0300

    refactor(main): finalized second project.

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("Hello world!")
+let firstName = "Charles" 
+let lastName = "Mello"
+let yearsOld = 21
+
+console.log("My name is: " + firstName + " " + lastName + "; I have " + yearsOld + " years old")
