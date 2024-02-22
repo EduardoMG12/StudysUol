b8abfaacf998ada2e6f27cb5fbc6eed39b7b4862
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-02-22 16:01:55 -0300

    refactor(main): added getElementById and inner input responses in html, finalized project five

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+const paragraph = document.getElementById("paragraph")
+
 let firstName = prompt("What your first name? ") 
 let lastName = prompt("What your second name? ") 
 let yearsOld = prompt("how old are you? ")
 
-console.log("My name is: " + firstName + " " + lastName + "; I have " + yearsOld + " years old")
+paragraph.innerHTML= ("My name is: " + firstName + " " + lastName + "; I have " + yearsOld + " years old")
