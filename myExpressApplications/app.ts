a499fcb53ec83f23c7f23143efd46d86da7f2221
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-03-22 17:22:49 -0300

    shore: add body-parser configurations

--- myExpressApplications/app.ts
+++ myExpressApplications/app.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import routes from './routes/index';
+import auth from './routes/auth'
+import bodyParser from "body-parser";
+
 
 const app = express();
 const port = 3000;
+
+//configure body-parser to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 // Configure Express for use EJS how your visualization mechanism.
 app.set('view engine', 'ejs'); 
@@ -13,6 +21,7 @@ app.set('views', './views');
 // Serve static files from the public folder
 app.use(express.static('public'));
 
+app.use('/auth', auth);
 
 // app is an instance of express.
 // METHOD is an HTTP request method, in lowercase, which is use after app.
