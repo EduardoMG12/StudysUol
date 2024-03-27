fa5dfe8a888ac83d19e08b42f9e6a5a729e70a1e
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-03-27 19:29:11 -0300

    feat: add socketio settings

--- HYPEChat/app.ts
+++ HYPEChat/app.ts
@@ -1,6 +1,26 @@
 import app from './config/server.ts';
+// import cors from 'cors';
+import { Server } from 'socket.io'
 
-app.listen(3000, () => {
+
+const server = app.listen(3000, () => {
+    console.log("")
     console.log("🚀 Server started 🚀")
     console.log("http://localhost:3000")
+    console.log("")
 })
+
+export const socketio = new Server(server, { cors: { origin: '*' } });
+
+
+socketio.on('connection', (socket) => {
+    console.log("");
+    console.log('😊 user connected 😊');
+    console.log("");
+
+    socket.on("disconnect", () => {
+        console.log("");
+        console.log('😊 user disconnect 😊');
+        console.log("");
+    });
+});
