c8c73fa4b8e55957baefd070428eac21e24a149c
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-03-27 19:30:55 -0300

    feat: create homeController

---
+++ HYPEChat/app/controllers/homeController.ts
@@ -0,0 +1,6 @@
+import type { Request, Response } from "express";
+
+export const homeController = (req:Request, res:Response) => {
+    const title = 'Multiroom Chat'
+    res.render(`index`, {title: title})
+}
